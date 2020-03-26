f0ec81ef4c1f4d774461289a0e6f2f868f167492
Dahish <61220555+TheDahish@users.noreply.github.com>
2020-03-26 12:04:36 +0200

    .

--- Database/src/DBApp.java
+++ Database/src/DBApp.java
@@ -1,3 +1,42 @@
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.Iterator;
 
 public class DBApp {
+	
+	public void init() {
+		
+	}
+	
+	public void createTable(String strTableName,
+			String strClusteringKeyColumn,
+			Hashtable<String, String> htblColNameType)throws DBAppException{
+		
+	}
+	
+	public void insertIntoTable(String strTableName,
+			 Hashtable<String,Object> htblColNameValue)
+			 throws DBAppException{
+		
+	}
+	public void updateTable(String strTableName,
+			 String strClusteringKey,
+			Hashtable<String,Object> htblColNameValue )
+			throws DBAppException {
+		
+	}
+	public void deleteFromTable(String strTableName,
+			 Hashtable<String,Object> htblColNameValue)
+			 throws DBAppException {
+		
+	}
+	public Iterator selectFromTable(SQLTerm[] arrSQLTerms,
+		String[] strarrOperators)
+		throws DBAppException {
+			Iterator i = null;
+			return i;
+	}
+
+	
+	
 }
